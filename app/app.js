0c4847656aa07a81361bf984bfb577b3b815baa4
MhassanAv <93055463+MhassanAv@users.noreply.github.com>
2021-11-30 04:02:52 +0200

    change 2

--- app/app.js
+++
@@ -1,9 +0,0 @@
-let getdata = async () => {
-    const req = await fetch("/");
-    try{
-        const wholeData = await req.json();
-        console.log(wholeData);
-    }catch(error){
-        console.log('error',error);
-    }
-}
